ee70ce613506d7b92057915547f20ec1859e6fc9
Javier Martinez <javi.martinez097@gmail.com>
2026-08-29 12:07:57 -0600

    -Refactorizacion del codigo, actualizacion de referencias y ubicacion de la solucion - Se agrego  "AppDbcontext" en la carpeta Data - Se creo la Interfaz "ITaskItemRepository.cs" - Se creo el repositorio "TaskItemRepository.cs" -Se agregaron las dependencias para SQLite -Se creo la carpeta Migrations para generar la base de datos

--- TaskManager.API/Program.cs
+++ TaskManager.API/Program.cs
@@ -1,7 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using Task_Manager.Domain.Interfaces;
+using Task_Manager.Infrastructure.Data;
+using Task_Manager.Infrastructure.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddScoped<ITaskItemRepository, TaskItemRepository>();
+//builder.Services.AddScoped<TaskItemService>();
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
---
+++ TaskManager.Infrastructure/Repositories/TaskItemRepository.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Task_Manager.Domain.Entities;
+using Task_Manager.Domain.Interfaces;
+using Task_Manager.Infrastructure.Data;
+
+namespace Task_Manager.Infrastructure.Repositories
+{
+    public class TaskItemRepository : ITaskItemRepository
+    {
+        private readonly AppDbContext _context;
+        public TaskItemRepository(AppDbContext context) => _context = context;
+
+        public async Task<List<TaskItem>> GetAllAsync() => await _context.TaskItems.ToListAsync();
+        public async Task<TaskItem?> GetByIdAsync(int id) => await _context.TaskItems.FindAsync(id);
+        public async Task AddAsync(TaskItem task) => await _context.TaskItems.AddAsync(task);
+        public void Update(TaskItem task) => _context.TaskItems.Update(task);
+        public void Delete(TaskItem task) => _context.TaskItems.Remove(task);
+        public async Task<bool> SaveChangesAsync() => await _context.SaveChangesAsync() > 0;
+
+    }
+}
